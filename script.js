9237ad599c265eedc84d42e059b68bdc9a2056e8
jbabalfi-arch <jbabalfi@gmail.com>
2026-05-10 00:35:59 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+﻿function showTab(tabId) {
+    document.querySelectorAll('.tab-content').forEach(tab => tab.classList.remove('active'));
+    document.querySelectorAll('.nav-link').forEach(link => link.classList.remove('active'));
+    document.getElementById(tabId).classList.add('active');
+    event.currentTarget.classList.add('active');
+}
